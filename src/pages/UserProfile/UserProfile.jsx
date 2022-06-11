306446e0516f989eaddbea22cedad70f98b141e4
hernandez-g <g5hernandez@yahoo.com>
2022-06-11 08:28:08 -0700

    edits for user bio

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,12 +1,12 @@
 import "./UserProfile.css"
 import { AiOutlineHeart } from "react-icons/ai"
+import { FaRegEdit } from "react-icons/fa"
 import { Link } from "react-router-dom";
 import FavoritesPage from "../FavoritesPage/FavoritesPage";
 
 export default function UserProfile({user}) {
 
 
-  // setUser();
 
   return (
     <>
@@ -16,11 +16,15 @@ export default function UserProfile({user}) {
       <div className="userProfile">
        <h3 className="userDetails">
          {user.name}
+         {user.username}
       </h3>
-        <Link className="edit-profile" to="/editprofile">Edit Profile</Link>
-         &nbsp; &nbsp; &nbsp;
+      <div className="linksProfile">
+        <Link className="edit-profile" to="/editprofile"><FaRegEdit /></Link>
         <Link className="favorties-link-profile" to="/favorites"><AiOutlineHeart className="favorites-page-icon"/></Link>
-        <p>{user.bio}</p>
+      </div>
+        <p className="bioUser">
+          {user.bio}
+        </p>
         </div>
         </div>
         <br />
